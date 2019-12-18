cc533f9c5402f5a580b99a1575a80b018e02b132
Vladi40k <nfk.if@i.ua>
2019-12-18 12:55:09 +0200

    ebana studija, new pararam, must be in uther branch, but fuck off

---
+++ Filters/ParametraizedFilter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop.Filters
+{
+    public abstract class ParametraizedFilter : IFilter
+    {
+        IParameters parameters;
+
+        public ParametraizedFilter(IParameters parameters) {
+            this.parameters = parameters;
+        }
+
+        public ParameterInfo[] GetParameters()
+        {
+            return parameters.GetDescription();
+        }
+
+        public Photo Process(Photo original, double[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+        public Photo Process(Photo original, double[] parameters) {
+            
+        }
+    }
+}
